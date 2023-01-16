3546a6ab318f1fadb4daf93f05029bed29313743
AnaLauraFFerraz <analaurafferraz@gmail.com>
2023-01-16 10:51:08 -0300

    post participants request

--- src/app.js
+++ src/app.js
@@ -11,16 +11,58 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const date = dayjs().format("hh:mm:ss")
+
 const PORT = 5000;
+app.listen(5000);
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
     await mongoClient.connect();
+    const db = mongoClient.db();
 } catch (err) {
     console.log(err.message);
 }
 
-const db = mongoClient.db();
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+    let checkUsername;
 
-app.listen(5000);
+    const usernameSchema = joi.object({
+        name: joi.string().required()
+    });
+
+    const usernameValidation = usernameSchema.validate({ name });
+
+    if (usernameValidation.error) {
+        return res.status(422).send(usernameValidation.error.details);
+    }
+
+    try {
+        checkUsername = await db.collection("participants").findOne({ name });
+    } catch {
+        checkUsername = false;
+        console.log("Erro na validação do nome do usuário")
+    }
+
+    if (checkUsername) {
+        return res.status(409).send("Esse nome de usuário já está sendo utilizado.");
+    }
+
+    try {
+        const participant = { name, lastStatus: Date.now() };
+
+        await db.collection("participants").insertOne(participant);
+        await db.collection("messages").insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: date,
+        });
+        res.status(201)
+    } catch {
+        console.log("Erro ao cadastrar usuário")
+    }
+})
