de8c077129c7cccb7cf3b23ad8c7ceb8539e5811
AnaLauraFFerraz <analaurafferraz@gmail.com>
2023-01-16 11:38:09 -0300

    post message request

--- src/app.js
+++ src/app.js
@@ -75,3 +75,42 @@ app.get("/participants", async (req, res) => {
         res.status(500).send("Erro ao buscar participantes")
     }
 })
+
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body;
+    const from = req.headers.user;
+
+    const time = dayjs(Date.now()).format("hh:mm:ss");
+
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid("private_message", "message").required()
+    })
+
+    const messageValidation = messageSchema.validate({ to, text, type });
+
+    if (messageValidation.error) {
+        return res.status(422).send(messageValidation.error.details);
+    }
+
+    try {
+        await db.collection("participants").findOne({ name: from })
+    } catch {
+        res.status(422).send("Usuário não encontrado")
+    }
+
+    try {
+        await db.collection("messages").insertOne({
+            to,
+            text,
+            type,
+            from,
+            time,
+        });
+        res.status(201)
+    } catch {
+        res.status(500)
+    }
+});
+
