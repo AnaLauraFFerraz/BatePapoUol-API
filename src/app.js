440ab60646a1715da23017a12074a993aebda42d
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-14 12:50:25 -0300

    feat: add data sanitization to input fields

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from 'joi';
 import dayjs from "dayjs";
+import stripHtml from "string-strip-html";
 
 dotenv.config();
 
@@ -28,30 +29,32 @@ const db = mongoClient.db();
 app.post("/participants", async (req, res) => {
     const { name } = req.body;
 
+    const sanitized_name = stripHtml(name).result.trim();
+
     const usernameSchema = joi.object({
         name: joi.string().required()
     });
 
-    const validation = usernameSchema.validate({ name });
+    const validation = usernameSchema.validate({ name: sanitized_name });
 
     if (validation.error) {
         return res.sendStatus(422);
     }
 
     try {
-        const isUserActive = await db.collection("participants").findOne({ name });
+        const isUserActive = await db.collection("participants").findOne({ name: sanitized_name });
         if (isUserActive) {
             return res.sendStatus(409);
         }
 
         const participants = {
-            name: name,
+            name: sanitized_name,
             lastStatus: Date.now()
         };
         await db.collection("participants").insertOne(participants);
 
         const message = {
-            from: name,
+            from: sanitized_name,
             to: "Todos",
             text: "entra na sala...",
             type: "status",
@@ -78,6 +81,9 @@ app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body;
     const from = req.headers.user;
 
+    const sanitized_to = stripHtml(to).result.trim();
+    const sanitized_text = stripHtml(text).result.trim();
+
     const time = dayjs(Date.now()).format("hh:mm:ss");
 
     const messageSchema = joi.object({
@@ -86,7 +92,7 @@ app.post("/messages", async (req, res) => {
         type: joi.string().valid("private_message", "message").required()
     })
 
-    const validation = messageSchema.validate({ to, text, type });
+    const validation = messageSchema.validate({ to: sanitized_to, text: sanitized_text, type });
 
     if (validation.error) {
         return res.sendStatus(422);
@@ -103,15 +109,15 @@ app.post("/messages", async (req, res) => {
 
     try {
         await db.collection("messages").insertOne({
-            to,
-            text,
+            to: sanitized_to,
+            text: sanitized_text,
             type,
             from,
             time,
         });
-        res.status(201);
+        res.sendStatus(201);
     } catch {
-        res.status(500);
+        res.sendStatus(500);
     }
 });
 
@@ -136,82 +142,32 @@ app.get("/messages", async (req, res) => {
         limit = 100;
     }
 
-    let userMessages = allMessages.filter((message) =>
-        message.user === user ||
-        message.from === user ||
-        message.to === "Todos" ||
-        message.to === user ||
-        message.type === "status"
-    );
+    const filteredMessages = allMessages.filter(message => {
+        return message.type === "message" || message.from === user || message.to === user || message.to === "Todos";
+    }).slice(-limit);
 
-    const limitedMessages = userMessages.slice(limit)
-    res.send(limitedMessages)
+    res.status(200).send(filteredMessages);
 })
 
 app.post("/status", async (req, res) => {
-    const user = req.headers.user;
-    const time = Date.now();
+    const { user } = req.headers;
 
     try {
-        const isUser = await db.collection("participants").findOne({ name: user });
-        if (!isUser) {
-            return res.sendStatus(422);
+        const updatedParticipant = await db.collection("participants").findOneAndUpdate(
+            { name: user },
+            { $set: { lastStatus: Date.now() } },
+            { returnDocument: "after" }
+        );
+
+        if (!updatedParticipant.value) {
+            res.sendStatus(400);
+        } else {
+            res.sendStatus(200);
         }
     } catch {
         res.sendStatus(500);
     }
-
-    const participantStatus = { name: user, lastStatus: time };
-
-    try {
-        await db.collection("participants").updateOne({ name: user }, { $set: participantStatus });
-        return res.sendStatus(200);
-    } catch {
-        res.sendStatus(500);
-    }
-})
-
-setInterval(
-    async () => {
-        const now = Date.now();
-        const time = dayjs(Date.now()).format("hh:mm:ss");
-
-        const inactiveUsers = await db.collection("participants").find({
-            lastStatus: { $lt: now - 10000 }
-        }).toArray();
-
-        inactiveUsers.forEach(async (user) => {
-            await db.collection("participants").deleteOne({ name: user.name });
-            await db.collection("messages").insertOne({
-                from: user.name,
-                to: "Todos",
-                text: "sai da sala...",
-                type: "status",
-                time: time
-            });
-        })
-    }, 15000)
-
-app.delete("/messages/:id", async (req, res) => {
-    const { id } = req.params;
-    const name = req.headers.user;
-
-    try {
-        const message = await db.collection("messages").findOne({ _id: ObjectId(id) });
-
-        if (!message) return res.sendStatus(404);
-
-        if (message.from !== name) return res.sendStatus(401);
-
-        await db.collection("messages").deleteOne({ _id: ObjectId(id) });
-
-        res.sendStatus(200);
-    } catch {
-        res.sendStatus(500);
-    }
 });
-
-app.listen(PORT);
 
 app.put("/messages/:id", async (req, res) => {
     const { id } = req.params;
@@ -247,3 +203,45 @@ app.put("/messages/:id", async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+app.delete("/messages/:id", async (req, res) => {
+    const { id } = req.params;
+    const name = req.headers.user;
+
+    try {
+        const message = await db.collection("messages").findOne({ _id: ObjectId(id) });
+
+        if (!message) return res.sendStatus(404);
+
+        if (message.from !== name) return res.sendStatus(401);
+
+        await db.collection("messages").deleteOne({ _id: ObjectId(id) });
+
+        res.sendStatus(200);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+setInterval(
+    async () => {
+        const now = Date.now();
+        const time = dayjs(Date.now()).format("hh:mm:ss");
+
+        const inactiveUsers = await db.collection("participants").find({
+            lastStatus: { $lt: now - 10000 }
+        }).toArray();
+
+        inactiveUsers.forEach(async (user) => {
+            await db.collection("participants").deleteOne({ name: user.name });
+            await db.collection("messages").insertOne({
+                from: user.name,
+                to: "Todos",
+                text: "sai da sala...",
+                type: "status",
+                time: time
+            });
+        })
+    }, 15000)
+
+app.listen(PORT);
